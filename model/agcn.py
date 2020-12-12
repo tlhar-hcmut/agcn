199e5fc6b057df9c10c55013d8ef86b805bbfea5
khoidd <khoidd@vng.com.vn>
2020-12-12 14:07:26 +0700

    fix gcn layer

--- model/agcn.py
+++ model/agcn.py
@@ -4,7 +4,7 @@ import torch
 import torch.nn as nn
 from torch.autograd import Variable
 from importer import import_class
-from initor import *
+from model import tools
 
 
 class UnitTcn(nn.Module):
@@ -31,92 +31,97 @@ class UnitTcn(nn.Module):
             track_running_stats=True,
         )
 
-        nn.init.constant_(bn.bias, 0)
-        nn.init.constant_(bn.weight, 1)
-
-        nn.init.constant_(conv.bias, 0)
-        nn.init.kaiming_normal_(conv.weight, mode='fan_out')
+        tools.init_bn(self.bn, 1)
+        tools.init_conv(self.conv)
 
     def forward(self, x):
         return self.bn(self.conv(x))
 
 
 class UnitGcn(nn.Module):
-    def __init__(self, in_channels, out_channels, A, coff_embedding=4, num_subset=3):
+    def __init__(self, in_channels, out_channels, mat_adj, coff_embedding=4, num_subset=3):
         super(UnitGcn, self).__init__()
-        self.inter_c = out_channels // coff_embedding
-        self.PA = nn.Parameter(torch.from_numpy(A.astype(np.float32)))
-        nn.init.constant_(self.PA, 1e-6)
-        self.A = Variable(
-            torch.from_numpy(A.astype(np.float32)),
-            requires_grad=False)
+        # Init constant
         self.num_subset = num_subset
-
+        self.inter_channels = out_channels // coff_embedding
+        # Init cnn layer
+        self.bn = nn.BatchNorm2d(out_channels)
+        self.relu = nn.ReLU()
+        self.soft = nn.Softmax(-2)
+        # Init gcn layler
+        self.mat_adj = Variable(
+            data=torch.from_numpy(mat_adj.astype(np.float32)),
+            requires_grad=False,
+        )
+        self.weight = nn.Parameter(
+            data=torch.from_numpy(mat_adj.astype(np.float32)),
+            requires_grad=True,
+        )
+        nn.init.constant_(self.weight, 1e-6)
+        # Init embedding layer
         self.conv_a = nn.ModuleList()
         self.conv_b = nn.ModuleList()
         self.conv_d = nn.ModuleList()
         for i in range(self.num_subset):
-            self.conv_a.append(nn.Conv2d(in_channels, self.inter_c, 1))
-            self.conv_b.append(nn.Conv2d(in_channels, self.inter_c, 1))
+            self.conv_a.append(nn.Conv2d(in_channels, self.inter_channels, 1))
+            self.conv_b.append(nn.Conv2d(in_channels, self.inter_channels, 1))
             self.conv_d.append(nn.Conv2d(in_channels, out_channels, 1))
 
         if in_channels != out_channels:
             self.conv_res = nn.Sequential(
                 nn.Conv2d(in_channels, out_channels, 1),
-                nn.BatchNorm2d(out_channels)
+                nn.BatchNorm2d(out_channels),
             )
         else:
             self.conv_res = lambda x: x
 
-        self.bn = nn.BatchNorm2d(out_channels)
-        self.soft = nn.Softmax(-2)
-        self.relu = nn.ReLU()
-
+        # Init weight
         for m in self.modules():
             if isinstance(m, nn.Conv2d):
-                init_conv(m)
+                tools.init_conv(m)
             elif isinstance(m, nn.BatchNorm2d):
-                init_bn(m, 1)
+                tools.init_bn(m, 1)
 
-        init_bn(self.bn, 1e-6)
+        tools.init_bn(self.bn, 1e-6)
 
         for i in range(self.num_subset):
-            init_conv_branch(self.conv_d[i], self.num_subset)
+            tools.init_conv_branch(self.conv_d[i], self.num_subset)
 
     def forward(self, x):
         N, C, T, V = x.size()
-        A = self.A.cuda(x.get_device())
-        A = A + self.PA
+        mat_adj = self.mat_adj.cuda(x.get_device())
+        mat_adj = mat_adj + self.weight
 
         y = None
         for i in range(self.num_subset):
-            A1 = self.conv_a[i](x)\
-                .permute(0, 3, 1, 2)\
-                .contiguous().view(N, V, self.inter_c * T)
-            A2 = self.conv_b[i](x).view(N, self.inter_c * T, V)
-            A1 = self.soft(torch.matmul(A1, A2) / A1.size(-1))  # N V V
-            A1 = A1 + A[i]
-            A2 = x.view(N, C * T, V)
-            z = self.conv_d[i](torch.matmul(A2, A1).view(N, C, T, V))
+            mat_embed_1 = self.conv_a[i](x)\
+                .permute(0, 3, 1, 2).contiguous()\
+                .view(N, V, self.inter_channels * T) # N-C,T,V -> N-V,T,C -> N-V,TC
+
+            mat_embed_2 = self.conv_b[i](x)\
+                .view(N, self.inter_channels * T, V) # N-C,T,V -> N-CT,V
+
+            mat_enhance = self.soft(torch.matmul(mat_embed_1, mat_embed_2) / A1.size(-1))  # N-V,V
+            
+            mat_inpt = x.view(N, C * T, V) # N-CT,V
+            mat_adapt = mat_adj[i] + mat_enhance # N-V,V
+
+            z = self.conv_d[i](torch.matmul(mat_inpt, mat_adapt).view(N, C, T, V)) # N-C,T,V
             y = z + y if y is not None else z
 
-        y = self.bn(y)
-        y += self.conv_res(x)
-        return self.relu(y)
+        return self.relu(self.bn(y) + self.conv_res(x))
 
 
 class UnitTcnGcn(nn.Module):
-    def __init__(self, in_channels, out_channels, A, stride=1, residual=True):
+    def __init__(self, in_channels, out_channels, mat_adj, stride=1, residual=True):
         super(UnitTcnGcn, self).__init__()
-        self.gcn1 = UnitGcn(in_channels, out_channels, A)
+        self.gcn1 = UnitGcn(in_channels, out_channels, mat_adj)
         self.tcn1 = UnitTcn(out_channels, out_channels, stride=stride)
         self.relu = nn.ReLU()
         if not residual:
             self.residual = lambda x: 0
-
         elif (in_channels == out_channels) and (stride == 1):
             self.residual = lambda x: x
-
         else:
             self.residual = UnitTcn(
                 in_channels=in_channels,
@@ -156,7 +161,7 @@ class Model(nn.Module):
 
         self.fc = nn.Linear(256, num_class)
         nn.init.normal_(self.fc.weight, 0, math.sqrt(2. / num_class))
-        init_bn(self.data_bn, 1)
+        tools.init_bn(self.data_bn, 1)
 
     def forward(self, x):
         N, C, T, V, M = x.size()
