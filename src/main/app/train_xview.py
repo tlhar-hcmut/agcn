29bbe0c5ed21c33282d54e15dd9a04d178043b0e
khoidd <khoidd@outlook.com>
2021-03-20 13:08:12 +0700

    add train_xview

---
+++ src/main/app/train_xview.py
@@ -0,0 +1,49 @@
+import torch
+from src.main.feeder.ntu import NtuFeeder
+from src.main.graph import NtuGraph
+from src.main.model.agcn import UnitAGCN
+from torch import nn
+from torch.autograd import Variable
+from torch.utils.data import DataLoader
+from tqdm.std import tqdm
+
+if __name__ == "__main__":
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+    feeder_train = NtuFeeder(
+        path_data="/data/preprocess/nturgb+d_skeletons/train_xview_joint.npy",
+        path_label="/data/preprocess/nturgb+d_skeletons/train_xview_label.pkl",
+    )
+
+    loader_train = DataLoader(
+        dataset=feeder_train,
+        batch_size=8,
+        shuffle=False,
+        num_workers=1,
+    )
+
+    feeder_test = NtuFeeder(
+        path_data="/data/preprocess/nturgb+d_skeletons/val_xview_joint.npy",
+        path_label="/data/preprocess/nturgb+d_skeletons/val_xview_label.pkl",
+    )
+
+    loader_test = DataLoader(
+        dataset=feeder_test,
+        batch_size=8,
+        shuffle=False,
+        num_workers=1,
+    )
+
+    loss = nn.CrossEntropyLoss()
+
+    model = UnitAGCN(num_class=12, cls_graph=NtuGraph)
+
+    for batch_idx, (data, label, index) in enumerate(tqdm(loader_test)):
+        print(type(data))
+        print(type(label))
+        print(type(index))
+        data = Variable(data.float().to(device), requires_grad=False)
+        label = Variable(label.long().to(device), requires_grad=False)
+        output = model(data)
+        print(data.get_device())
+        break
