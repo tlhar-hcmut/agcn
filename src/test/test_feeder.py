242b2de350ace5b1be4f8b7878872b57ee1d09ac
khoidd <khoidd@outlook.com>
2021-03-20 11:48:15 +0700

    add test feeder

--- src/test/test_feeder.py
+++ src/test/test_feeder.py
@@ -1,15 +1,15 @@
 import unittest
 
-from main.feeder.ntu import NtuFeeder
+from src.main.feeder.ntu import NtuFeeder
 
 
 class TestFeeder(unittest.TestCase):
     def setUp(self) -> None:
-        feeder = NtuFeeder(
-            path_data="",
-            path_label="",
-            ls_class
+        dir_ds = "/data/preprocess/nturgb+d_skeletons"
+        self.feeder = NtuFeeder(
+            path_data=dir_ds + "/train_xsub_joint.npy",
+            path_label=dir_ds + "/train_xsub_label.pkl",
         )
 
-    def test(self):
-        pass
+    def test_getitem(self):
+        self.assertEqual(self.feeder.__getitem__(0)[0].shape, (3, 300, 25, 2))
