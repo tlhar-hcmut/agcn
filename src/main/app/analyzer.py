8077103e9b020e222ec172969a43060fc8a17098
khoidd <khoidd@outlook.com>
2021-03-20 13:10:12 +0700

    update code

--- src/main/app/analyzer.py
+++ src/main/app/analyzer.py
@@ -1,7 +1,9 @@
-from src.main.config import cfg_ds_v1
-from src.main.util import pprinter
-from src.main.generator import util
 import os
+
+from src.main.config import cfg_ds_v1
+from src.main.generator import util
+from src.main.util import pprinter
+
 
 def list_info(chosen_classes):
     '''
@@ -9,42 +11,43 @@ def list_info(chosen_classes):
     '''
     # S015C003P025R002A017
     setup_numbers = set()
-    camera_ids = set() 
+    camera_ids = set()
     performer_ids = set()
     replication_numbers = set()
-    action_classes=set()
+    action_classes = set()
 
     for filename in os.listdir(input_data_raw):
         extracted_name = util.read_meta_data(filename)
-        if (extracted_name['action_class'] not in chosen_classes): 
-            continue 
+        if (extracted_name['action_class'] not in chosen_classes):
+            continue
 
         setup_numbers.add(extracted_name['setup_number'])
         camera_ids.add(extracted_name['camera_id'])
         performer_ids.add(extracted_name['performer_id'])
         replication_numbers.add(extracted_name['replication_number'])
         action_classes.add(extracted_name['action_class'])
-    
+
     return {
-            'setup_number':setup_numbers,
-            'camera_id':camera_ids,
-            'performer_id': performer_ids,
-            'replication_number':replication_numbers,
-            'action_class':action_classes
-            }
-   
+        'setup_number': setup_numbers,
+        'camera_id': camera_ids,
+        'performer_id': performer_ids,
+        'replication_number': replication_numbers,
+        'action_class': action_classes
+    }
+
 
 def check_full_data(chosen_classes):
     '''
     Check if each class has full options (full setups, full person, full camera,...)
     '''
     for class_ in chosen_classes:
-        infos=list_info([class_])
-        if  infos['setup_number'].__len__!= 32\
-            or infos['camera_id'].__len__!= 3\
-            or infos['performer_id'].__len__!= 106\
-            or infos['replication_number'].__len__!= 2:
+        infos = list_info([class_])
+        if infos['setup_number'].__len__ != 32\
+                or infos['camera_id'].__len__ != 3\
+                or infos['performer_id'].__len__ != 106\
+                or infos['replication_number'].__len__ != 2:
             pprinter.pp(infos, '[check_full_data] class {} is not full data.'.format(infos['action_class']))
+
 
 if __name__ == "__main__":
 
@@ -52,8 +55,6 @@ if __name__ == "__main__":
     input_data_raw = cfg_ds_v1.path_data_raw
 
     infos = list_info(chosen_classes)
-    pprinter.pp(infos,"View of all classes")
+    pprinter.pp(infos, "View of all classes")
 
-    check_full_data(chosen_classes)    
-
-
+    check_full_data(chosen_classes)
