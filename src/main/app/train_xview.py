75cb909f5906f4a7edc2885ded0becfadcd8b686
thucth-qt <thucth.qt@gmail.com>
2021-03-27 15:24:38 +0700

    train model demo

--- src/main/app/train_xview.py
+++ src/main/app/train_xview.py
@@ -6,13 +6,17 @@ from torch import nn
 from torch.autograd import Variable
 from torch.utils.data import DataLoader
 from tqdm.std import tqdm
+from src.main.config import cfg_ds_v1
+import numpy as np
+import torch.optim as optim
+import matplotlib.pyplot as plt
 
 if __name__ == "__main__":
-    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-
+    device = torch.device("cpu")
+    
     feeder_train = NtuFeeder(
-        path_data="/data/preprocess/nturgb+d_skeletons/train_xview_joint.npy",
-        path_label="/data/preprocess/nturgb+d_skeletons/train_xview_label.pkl",
+        path_data= cfg_ds_v1.path_data_preprocess+"/train_xview_joint.npy",
+        path_label=cfg_ds_v1.path_data_preprocess+"/train_xview_label.pkl",
     )
 
     loader_train = DataLoader(
@@ -22,25 +26,36 @@ if __name__ == "__main__":
         num_workers=1,
     )
 
-    feeder_test = NtuFeeder(
-        path_data="/data/preprocess/nturgb+d_skeletons/val_xview_joint.npy",
-        path_label="/data/preprocess/nturgb+d_skeletons/val_xview_label.pkl",
-    )
-
-    loader_test = DataLoader(
-        dataset=feeder_test,
-        batch_size=8,
-        shuffle=False,
-        num_workers=1,
-    )
-
-    loss = nn.CrossEntropyLoss()
-
+    loss = nn.CrossEntropyLoss().cuda("gpu")
+    
+    #declarations:
     model = UnitAGCN(num_class=12, cls_graph=NtuGraph)
+    model = model.cuda()
+    optimizer = optim.SGD(model.parameters(), lr=0.004)
+    losses =[]
+    model.train()
+    for epoch in range(5):
+        for batch_idx, (data, label, index) in enumerate(tqdm(loader_train)):
+            data = Variable(data.float().to(device), requires_grad=False).cuda()
+            label = Variable(label.long().to(device), requires_grad=False).cuda()
+            #forward
+            output_ = model(data)
+            #convert label nominal into one-hot
+            # label_onehot = torch.zeros((label.data.shape[0], 12))
+            # row = torch.arange(0, label.data.shape[0], dtype=torch.long)
+            # label_onehot[row, label.data] = 1
+            loss_ = loss(output_, label)
 
-    for batch_idx, (data, label, index) in enumerate(tqdm(loader_test)):
-        data = Variable(data.float().to(device), requires_grad=False)
-        label = Variable(label.float().to(device), requires_grad=False)
+            #backward
+            optimizer.zero_grad()
+            loss_.backward()
+            optimizer.step()
 
-        output = model(data)
-        # break
+            losses.append(loss_.data.item())
+        
+            if (len(losses)%100!=0): continue
+        plt.plot(losses)
+        plt.xlabel('epoch')
+        plt.ylabel('loss')
+        plt.savefig("output_train/losses{}.png".format(epoch))
+    torch.save(model.state_dict(), "output_train/weight/model.pt")
