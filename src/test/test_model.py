75c4a5e8b34d5cd749be3e83456a770a96eca4ef
khoidd <khoidd@vng.com.vn>
2021-03-31 13:25:10 +0700

    update project: double param

--- src/test/test_model.py
+++ src/test/test_model.py
@@ -3,17 +3,21 @@ import unittest
 import numpy as np
 import src.main.model as M
 from src.main.graph import NtuGraph
-
+from torchsummary import summary
+import torch
 
 class TestModel(unittest.TestCase):
     def test_agcn(self):
-        model = M.UnitAGCN(cls_graph=NtuGraph)
+        # C, T, V, M = 
+        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        model = M.UnitAGCN(num_class=12, cls_graph=NtuGraph).to(device)
+        summary(model, input_size=(3, 300, 25, 2))
 
-    def test_gcn(self):
-        model = M.UnitGCN(3, 64, np.ones((25, 25)))
+    # def test_gcn(self):
+    #     model = M.UnitGCN(3, 64, np.ones((25, 25)))
 
-    def test_tcn(self):
-        model = M.UnitTCN(64, 64)
+    # def test_tcn(self):
+    #     model = M.UnitTCN(64, 64)
 
-    def test_tgcn(self):
-        model = M.UnitTGCN(3, 64, np.ones((25, 25)))
+    # def test_tgcn(self):
+    #     model = M.UnitTGCN(3, 64, np.ones((25, 25)))
