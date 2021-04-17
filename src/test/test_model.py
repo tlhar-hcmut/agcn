203e6c40f5b8ff68d2b127dcd1382564a8c7fc11
khoidd <khoidd@outlook.com>
2021-04-17 09:11:09 +0700

    update code

--- src/test/test_model.py
+++ src/test/test_model.py
@@ -2,26 +2,23 @@ import unittest
 
 import numpy as np
 import src.main.model as M
+import torch
 from src.main.graph import NtuGraph
 from torchsummary import summary
-import torch
+
 
 class TestModel(unittest.TestCase):
     def test_agcn(self):
-        # C, T, V, M = 
+        # C, T, V, M =
         device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         model = M.Net(device, num_class=12, cls_graph=NtuGraph).to(device)
         summary(model, input_size=(3, 300, 25, 2))
 
-    # def test_gcn(self):
-    #     model = M.UnitGCN(3, 64, np.ones((25, 25)))
+    def test_gcn(self):
+        model = M.UnitGCN(3, 64, np.ones((25, 25)))
 
-    # def test_tcn(self):
-    #     model = M.UnitTCN(64, 64)
+    def test_tcn(self):
+        model = M.UnitTCN(64, 64)
 
-    # def test_tgcn(self):
-    #     model = M.UnitTGCN(3, 64, np.ones((25, 25)))
-
-if __name__ == "__main__":
-    a = TestModel()
-    a.test_agcn()
+    def test_tgcn(self):
+        model = M.UnitTGCN(3, 64, np.ones((25, 25)))
