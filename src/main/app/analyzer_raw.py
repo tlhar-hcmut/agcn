845b954c0acc51eb3322232d66c1e5037ef6b848
thucth-qt <thucth.qt@gmail.com>
2021-04-22 15:44:18 +0700

    update project

--- src/main/app/analyzer_raw.py
+++ src/main/app/analyzer_raw.py
@@ -5,7 +5,7 @@ from src.main.generator import util
 from src.main.util import pprinter
 
 
-def list_info(chosen_classes):
+def list_info(chosen_classes, path_sample_ignore):
     '''
     List out all class, subject, numbody,.... of chosen classes
     '''
@@ -15,8 +15,16 @@ def list_info(chosen_classes):
     performer_ids = set()
     replication_numbers = set()
     action_classes = set()
+    num_samples=0
+
+    ls_sample_ignore=[]
+    if path_sample_ignore!=None:
+        with open(path_sample_ignore, 'r') as f:
+            ls_sample_ignore =[line.strip()+".skeleton" for line in f.readlines()]
 
     for filename in os.listdir(input_data_raw):
+        if filename in ls_sample_ignore:
+            continue
         extracted_name = util.read_meta_data(filename)
         if (extracted_name['action_class'] not in chosen_classes):
             continue
@@ -26,22 +34,24 @@ def list_info(chosen_classes):
         performer_ids.add(extracted_name['performer_id'])
         replication_numbers.add(extracted_name['replication_number'])
         action_classes.add(extracted_name['action_class'])
+        num_samples+=1
 
     return {
         'setup_number': setup_numbers,
         'camera_id': camera_ids,
         'performer_id': performer_ids,
         'replication_number': replication_numbers,
-        'action_class': action_classes
+        'action_class': action_classes,
+        'num_sample': num_samples
     }
 
 
-def check_full_data(chosen_classes):
+def check_full_data(chosen_classes, ls_sample_ignore):
     '''
     Check if each class has full options (full setups, full person, full camera,...)
     '''
     for class_ in chosen_classes:
-        infos = list_info([class_])
+        infos = list_info([class_], ls_sample_ignore)
         if infos['setup_number'].__len__ != 32\
                 or infos['camera_id'].__len__ != 3\
                 or infos['performer_id'].__len__ != 106\
@@ -53,8 +63,9 @@ if __name__ == "__main__":
 
     chosen_classes = cfg_ds_v1.ls_class
     input_data_raw = cfg_ds_v1.path_data_raw
+    sample_ignore=cfg_ds_v1.path_data_ignore
 
-    infos = list_info(chosen_classes)
+    infos = list_info(chosen_classes, sample_ignore)
     pprinter.pp(infos, "View of all classes")
 
-    check_full_data(chosen_classes)
+    check_full_data(chosen_classes, sample_ignore)
