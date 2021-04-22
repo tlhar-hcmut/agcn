fe8070bb6aa9b6f5da44648706a10ee85d91d0cc
thucth-qt <thucth.qt@gmail.com>
2021-04-22 17:26:39 +0700

    update class name

--- src/main/generator/util.py
+++ src/main/generator/util.py
@@ -8,7 +8,7 @@ class ClassOrder:
 
     @staticmethod
     def reorder(list_class:list):
-        max_idx=1
+        max_idx=0
 
         for class_ in list_class:
             if class_ not in ClassOrder.new_order_map:
