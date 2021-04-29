ca191ab78a9d7bdb70cd89d2c00c9ab58e169689
khoidd <khoidd@outlook.com>
2021-04-29 19:09:17 +0700

    update code

--- src/test/test_feeder.py
+++ src/test/test_feeder.py
@@ -1,6 +1,8 @@
 import unittest
 
+import numpy as np
 from src.main.feeder.ntu import NtuFeeder
+from torch.utils import data
 
 
 class TestFeeder(unittest.TestCase):
@@ -9,7 +11,26 @@ class TestFeeder(unittest.TestCase):
         self.feeder = NtuFeeder(
             path_data=dir_ds + "/train_xsub_joint.npy",
             path_label=dir_ds + "/train_xsub_label.pkl",
+            random_shift=True,
+            random_speed=True,
         )
 
     def test_getitem(self):
         self.assertEqual(self.feeder.__getitem__(0)[0].shape, (3, 300, 25, 2))
+
+    def test_getitem(self):
+        data_numpy = self.feeder.__getitem__(0)[0]
+        data_numpy_old = np.copy(data_numpy)
+        speed: int = 2
+        indices = np.floor(np.arange(0, 300 * speed, speed)).astype(np.int)
+        max_index = np.floor(300 / speed).astype(np.int)
+        indices[max_index:] = 0
+        data_numpy[:, :, :, :] = data_numpy[:, indices, :, :]
+        data_numpy[:, max_index:, :, :] = 0
+
+        self.assertEqual((data_numpy[:, 0, :, :] == data_numpy_old[:, 0, :, :]).all(), True)
+        self.assertEqual((data_numpy[:, 1, :, :] == data_numpy_old[:, 2, :, :]).all(), True)
+        self.assertEqual((data_numpy[:, 2, :, :] == data_numpy_old[:, 4, :, :]).all(), True)
+        self.assertEqual((data_numpy[:, 1, :, :] == data_numpy_old[:, 1, :, :]).sum(), 3)
+        self.assertEqual((data_numpy[:, 10, :, :] == data_numpy_old[:, 10, :, :]).sum(), 3)
+
