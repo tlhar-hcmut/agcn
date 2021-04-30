bff24e5648547dac74761c621699c49b1ff5a327
thucth-qt <thucth.qt@gmail.com>
2021-04-30 17:26:17 +0700

    update

--- src/main/model/stream_temporal/attension_fusion.py
+++ src/main/model/stream_temporal/attension_fusion.py
@@ -5,11 +5,12 @@ import torch.nn.functional as F
 class AttentionFusion(nn.Module):
     def __init__(self, input_size, dropout, **kwargs):
         super(AttentionFusion, self).__init__(**kwargs)
+        self.input_size = input_size
         self.dropout = nn.Dropout(dropout)
         self.ln = nn.LayerNorm(input_size)
 
     def forward(self, *X):
-        X_out = torch.empty(X[0].size)
+        X_out = torch.empty(self.input_size)
         
         for X_ in X:
             X_out+=self.dropout(X_)
