79466d4c1e81fa446e563b72f5d602a22200ae2e
thucth-qt <thucth.qt@gmail.com>
2021-05-01 01:57:23 +0700

    add_fusion_linear

--- src/main/model/stream_temporal/attension_fusion.py
+++ src/main/model/stream_temporal/attension_fusion.py
@@ -3,31 +3,26 @@ import torch
 import torch.nn.functional as F
 
 class AttentionFusion(nn.Module):
-    def __init__(self, input_size, dropout, **kwargs):
+    def __init__(self, input_size, num_head=3, dropout=0, **kwargs):
         super(AttentionFusion, self).__init__(**kwargs)
         self.input_size = input_size
-        self.dropout = nn.Dropout(dropout)
-        self.ln = nn.LayerNorm(input_size)
+        # self.dropout = nn.Dropout(dropout)
+        # self.ln = nn.LayerNorm(input_size)
 
-        self.W_o = nn.Linear(input_size[-1]*input_size[-2], input_size[-1])
+        len_feature = input_size[-1]
+        self.W_o = nn.Linear(num_head*len_feature, len_feature)
 
 
     def forward(self, *X):
-        X_out = torch.empty(X[0].size()).to(X[0].get_device())
+        # X_out = torch.empty(X[0].size()).to(X[0].get_device())
         
-        for X_ in X:
-            X_out+=self.dropout(X_)
+        # for X_ in X:
+        #     X_out+=self.dropout(X_)
 
-        return X_out
+        # return X_out
 
+        # N, len_seq, num_head*len_feature
+        output_concat = torch.cat(X, dim=-1)
+        output_concat = self.W_o(output_concat)
 
-
-        # output_concat = torch.cat(X, dim=1)
-
-        # # N, num_head*len_seq, len_feature => N, len_feature, num_head*len_seq
-        # output_concat = output_concat.permute(0, 2, 1)
-        # output_concat = self.W_o(output_concat)
-
-        # #  N, len_feature, len_seq => N, len_seq, len_feature
-        # output_concat = output_concat.permute(0, 2, 1)
-        # return self.ln(output_concat)
+        return output_concat
