d229073efb9e0fd25ee376e3f4e17de27920be13
thucth-qt <thucth.qt@gmail.com>
2021-04-30 16:09:17 +0700

    update

--- src/main/config/cfg_train.py
+++ src/main/config/cfg_train.py
@@ -1,2 +1,2 @@
-# output_train = "/content/gdrive/Shareddrives/Thesis/result_train/temporal_stream"
-output_train = "output/train/temporal"
+output_train = "/content/gdrive/Shareddrives/Thesis/result_train/temporal_stream"
+# output_train = "output/train/temporal"
