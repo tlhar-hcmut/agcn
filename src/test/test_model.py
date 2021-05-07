48cb9d4dfa49b3cc810fc40d88e43af15ed5c804
khoidd <khoidd@outlook.com>
2021-05-07 14:31:59 +0700

    update spatial stream

--- src/test/test_model.py
+++ src/test/test_model.py
@@ -1,42 +1,42 @@
 import unittest
+from collections import OrderedDict
 
 import numpy as np
 import src.main.model as M
-from src.main.graph import NtuGraph
 import torch
+from src.main.graph import NtuGraph
 from torchsummary import summary
 
 
 class TestModel(unittest.TestCase):
     def test_tk_model(self):
         model = M.TKNet(cls_graph=NtuGraph)
-        summary(model.to('cuda'), input_size=(3, 300, 25, 2))
+        summary(model.to("cuda"), input_size=(3, 300, 25, 2))
 
     def test_stream_temporal(self):
         model = M.StreamTemporalGCN(input_size=(3, 300, 25, 2), cls_graph=NtuGraph)
-        summary(model.to('cuda'), input_size=(3, 300, 25, 2))
+        summary(model.to("cuda"), input_size=(3, 300, 25, 2))
 
     def test_stream_spatial(self):
-        model = M.StreamSpatialGCN(input_size=(3, 300, 25, 2), cls_graph=NtuGraph)
-        summary(model.to('cuda'), input_size=(3, 300, 25, 2))
+        model = M.StreamSpatialGCN()
 
     def test_tagcn(self):
         model = M.UnitTAGCN(3, 64, np.ones((25, 25)))
-        summary(model.to('cuda'), input_size=(3, 300, 25))
+        summary(model.to("cuda"), input_size=(3, 300, 25))
 
     def test_agcn(self):
         model = M.UnitAGCN(3, 64, np.ones((25, 25)))
-        summary(model.to('cuda'), input_size=(3, 300, 25))
+        summary(model.to("cuda"), input_size=(3, 300, 25))
 
     def test_tcn(self):
         model = M.UnitTCN(3, 32)
-        summary(model.to('cuda'), input_size=(3, 300, 25))
+        summary(model.to("cuda"), input_size=(3, 300, 25))
 
     def test_self_attention(self):
         model = M.SelfAttention(16, 32)
-        summary(model.to('cuda'), input_size=(300, 16))
+        summary(model.to("cuda"), input_size=(300, 16))
 
     def test_multi_attention(self):
-        model = M.MultiHeadAttention(1,300, 16, 32)
-        summary(model.to('cuda'), input_size=(300, 16))
+        model = M.MultiHeadAttention(1, 300, 16, 32)
+        summary(model.to("cuda"), input_size=(300, 16))
 
