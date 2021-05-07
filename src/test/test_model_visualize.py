305980d45758447635cc6626ea86bfe02a1038f6
thucth <thucth.qt@gmail.com>
2021-05-07 05:49:47 +0000

    update visualize general

--- src/test/test_model_visualize.py
+++ src/test/test_model_visualize.py
@@ -10,14 +10,14 @@ from xcommon import xfile
 from .base_test import BaseTestCase
 
 class TestModelVisualize(BaseTestCase):
-    def test_tk_model(self):
-        model = M.TKNet(num_class=12, cls_graph=NtuGraph)
-        self.summary_to_file("TkModel",model.to('cuda'), (3, 300, 25, 2), depth=5)
+    # def test_tk_model(self):
+    #     model = M.TKNet(num_class=12, cls_graph=NtuGraph)
+    #     self.summary_to_file("TkModel",model.to('cuda'), (3, 300, 25, 2), depth=5)
 
-    def test_tk_spatial(self):
-        model = M.TKNet(num_class=12, stream=[0], cls_graph=NtuGraph)
-        self.summary_to_file("Spatial",model.to('cuda'), (3, 300, 25, 2))
+    # def test_tk_spatial(self):
+    #     model = M.TKNet(num_class=12, stream=[0], cls_graph=NtuGraph)
+    #     self.summary_to_file("Spatial",model.to('cuda'), (3, 300, 25, 2))
 
     def test_tk_temporal(self):
         model = M.TKNet(num_class=12, stream=[1], cls_graph=NtuGraph)
-        self.summary_to_file("Temporal",model.to('cuda'), (3, 300, 25, 2))
+        self.summary_to_file(title="Temporal",model = model.to('cuda'), input_size = (3, 300, 25, 2), depth=7)
