7d25247ce35c17121656efdf549c2791e7706475
thucth-qt <thucth.qt@gmail.com>
2021-05-08 12:22:33 +0700

    4unit

--- src/main/config/cfg_train.py
+++ src/main/config/cfg_train.py
@@ -1,10 +1,10 @@
-output_train = "/content/gdrive/Shareddrives/Thesis/result_train/temporal_stream/batch_aggrigate/26_joints_75F_150Frame/first"
+output_train = "/content/gdrive/Shareddrives/Thesis/result_train/temporal_stream/batch_aggrigate/26_joints_75F_150Frame/second_4unit"
 # output_train    =   "output/train/temporal"
 batch_size      =   8
 stream          =   [1]
 input_size =(3, 150, 26, 2) 
 #temporal
-len_feature_new = [78, 78, 128, 128, 128]
-num_block = 3
+len_feature_new = [78, 78, 78, 128, 128]
+num_block = 4
 dropout = 0.2
 num_head = 5
