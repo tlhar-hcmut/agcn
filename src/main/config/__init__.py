0f9c0ceedb669c4f3b45997e183b4e5e978f678a
colab <thucth.qt@gmail.com>
2021-07-14 18:10:14 +0000

    update full 8 configs

--- src/main/config/__init__.py
+++ src/main/config/__init__.py
@@ -2,15 +2,18 @@
 #                     Sequence                                    #
 ###################################################################
 
-# from .cfg_ds import config_daily_26_sequent_xview as cfg_ds
-# from .cfg_ds import config_daily_26_sequent_xsub as cfg_ds
 # from .cfg_ds import config_daily_25_sequent_xview as cfg_ds
 # from .cfg_ds import config_daily_25_sequent_xsub as cfg_ds
+# from .cfg_ds import config_daily_26_sequent_xview as cfg_ds
+# from .cfg_ds import config_daily_26_sequent_xsub as cfg_ds
 
 ###################################################################
 #                     Parallel                                    #
 ###################################################################
-from .cfg_ds import config_daily_25_parallel_xview as cfg_ds
+# from .cfg_ds import config_daily_25_parallel_xview as cfg_ds
+# from .cfg_ds import config_daily_25_parallel_xsub as cfg_ds
+# from .cfg_ds import config_daily_26_parallel_xview as cfg_ds
+# from .cfg_ds import config_daily_26_parallel_xsub as cfg_ds
 
 
 ###################################################################
--- src/main/config/cfg_ds.py
+++ src/main/config/cfg_ds.py
@@ -59,7 +59,7 @@ xview = BenchmarkConfig(
 ###################################################################
 
 config_daily_25_sequent_xview = TKHARConfig(
-    name       ="config_daily_25_sequent",       
+    name       ="config_daily_25_sequent_xview",       
     desc        ="",
 
     benchmark="xview",
@@ -83,9 +83,8 @@ config_daily_25_sequent_xview = TKHARConfig(
     num_head        =8,
 )
 
-
 config_daily_25_sequent_xsub = TKHARConfig(
-    name       ="config_daily_25_sequent",       
+    name       ="config_daily_25_sequent_xsub",       
     desc        ="",
 
     benchmark="xsub",
@@ -110,7 +109,7 @@ config_daily_25_sequent_xsub = TKHARConfig(
 )
 
 config_daily_26_sequent_xview = TKHARConfig(
-    name       ="config_daily_26_sequent",       
+    name       ="config_daily_26_sequent_xview",       
     desc        ="",
 
     benchmark="xview",
@@ -135,7 +134,7 @@ config_daily_26_sequent_xview = TKHARConfig(
 )
 
 config_daily_26_sequent_xsub = TKHARConfig(
-    name       ="config_daily_26_sequent",       
+    name       ="config_daily_26_sequent_xsub",       
     desc        ="",
 
     benchmark="xsub",
@@ -164,7 +163,7 @@ config_daily_26_sequent_xsub = TKHARConfig(
 #                     Parallel                                    #
 ###################################################################
 config_daily_25_parallel_xview = TKHARConfig(
-    name       ="config_daily_25_parallel",       
+    name       ="config_daily_25_parallel_xview",       
     desc        ="",
 
     benchmark="xview",
@@ -184,6 +183,88 @@ config_daily_25_parallel_xview = TKHARConfig(
     
     #configs for temporal stream
     input_size_temporal = (3, 300, 25, 2),
+    len_feature_new = [256, 256, 512],
+    num_block       =2,
+    dropout         =0.2,
+    num_head        =8,
+)
+
+config_daily_25_parallel_xsub = TKHARConfig(
+    name       ="config_daily_25_parallel_xsub",       
+    desc        ="",
+
+    benchmark="xsub",
+
+    path_data_raw="/data/extracts/nturgb+d_skeletons",
+    path_data_ignore="/data/extracts/samples_with_missing_skeletons.txt",
+    path_visualization="output/visualization/",
+    path_data_preprocess="/data_preprocess_daily_25/preprocess/nturgb+d_skeletons_reorder",
+    ls_benmark=[xview, xsub],
+    num_joint=25,
+
+    #common configs
+    output_train    = "/content/gdrive/Shareddrives/Thesis/result_train/temporal_stream/xxxxxxx",
+    input_size      = (3, 300, 25, 2),
+    optim_cfg       ={},
+    stream          =[0,1],
+    
+    #configs for temporal stream
+    input_size_temporal = (3, 300, 25, 2),
+    len_feature_new = [256, 256, 512],
+    num_block       =2,
+    dropout         =0.2,
+    num_head        =8,
+)
+
+config_daily_26_parallel_xview = TKHARConfig(
+    name       ="config_daily_26_parallel_xview",       
+    desc        ="",
+
+    benchmark="xview",
+
+    path_data_raw="/data/extracts/nturgb+d_skeletons",
+    path_data_ignore="/data/extracts/samples_with_missing_skeletons.txt",
+    path_visualization="output/visualization/",
+    path_data_preprocess="/data_preprocess_daily_26/preprocess/nturgb+d_skeletons_reorder",
+    ls_benmark=[xview, xsub],
+    num_joint=26,
+
+    #common configs
+    output_train    = "/content/gdrive/Shareddrives/Thesis/result_train/temporal_stream/xxxxxxx",
+    input_size      = (3, 300, 26, 2),
+    optim_cfg       ={},
+    stream          =[0,1],
+    
+    #configs for temporal stream
+    input_size_temporal = (3, 300, 26, 2),
+    len_feature_new = [256, 256, 512],
+    num_block       =2,
+    dropout         =0.2,
+    num_head        =8,
+)
+
+config_daily_26_parallel_xsub = TKHARConfig(
+    name       ="config_daily_26_parallel_xsub",       
+    desc        ="",
+
+    benchmark="xsub",
+
+    path_data_raw="/data/extracts/nturgb+d_skeletons",
+    path_data_ignore="/data/extracts/samples_with_missing_skeletons.txt",
+    path_visualization="output/visualization/",
+    path_data_preprocess="/data_preprocess_daily_26/preprocess/nturgb+d_skeletons_reorder",
+    ls_benmark=[xview, xsub],
+    num_joint=26,
+
+    #common configs
+    # output_train    = "/content/gdrive/Shareddrives/Thesis/result_train/temporal_stream/xxxxxxx",
+    output_train    = "output_26sub",
+    input_size      = (3, 300, 26, 2),
+    optim_cfg       ={},
+    stream          =[0,1],
+    
+    #configs for temporal stream
+    input_size_temporal = (3, 300, 26, 2),
     len_feature_new = [256, 256, 512],
     num_block       =2,
     dropout         =0.2,
